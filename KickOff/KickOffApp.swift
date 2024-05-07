8dabf7586f6086aca4d704cdc952c699e156c2f8
anubhavTechrange <anubhav@techrange.co>
2024-05-08 00:44:12 +0530

    Initial Commit

---
+++ KickOff/KickOffApp.swift
@@ -0,0 +1,17 @@
+//
+//  KickOffApp.swift
+//  KickOff
+//
+//  Created by Anubhav Rawat on 08/05/24.
+//
+
+import SwiftUI
+
+@main
+struct KickOffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
